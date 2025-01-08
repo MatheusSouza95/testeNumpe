d9cf4f931268aab4168a5eb013b5b6b8e9cdaca8
Matheus Martins <matheusmart95@gmail.com>
2025-01-08 19:46:58 -0300

    #01 - Commit Inicial

---
+++ src/main/java/com/example/numpe/NumpeApplication.java
@@ -0,0 +1,13 @@
+package com.example.numpe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumpeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NumpeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/numpe/NumpeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.numpe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NumpeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
